c2f1d8f47010a54a47cdfefc466873762278ae93
AgesilausDrako <agesilaus@yahoo.com>
2019-01-19 22:13:48 -0500

    added jwt-decode

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
+
 import { GET_ERRORS } from './types';
 
 // Register User
@@ -13,3 +15,22 @@ export const registerUser = (userData, history) => dispatch => {
             })
         );
 };
+
+// Login - Get User Token
+export const loginUser = userData => dispatch => {
+    axios
+        .post('/api/user/login', userData)
+        .then(res => {
+            // Save to localStorage
+            const { token } = res.data;
+            // Set token to local storage
+            localStorage.setItem('jwtToken', token);
+            // Set token to Auth header
+            setAuthToken(token);
+        })
+        .catch(err => 
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+        }));
+}
